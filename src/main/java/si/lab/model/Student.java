c3207cf38d47c63a25dc603196377b2a59c1bfa1
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-04-04 10:33:20 +0200

    initialize list of grades

--- src/main/java/si/lab/model/Student.java
+++ src/main/java/si/lab/model/Student.java
@@ -1,6 +1,7 @@
 package si.lab.model;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class Student {
     private List<Grade> grades;
 
     public Student() {
+        grades = new ArrayList<>();
     }
 
     public long getIndex() {
