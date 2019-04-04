a466cf5b6f623f95ef78ab0bd661d8208d983817
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-04-04 16:11:56 +0200

    remove reduntant import

--- src/main/java/si/lab/rest/StudentsCollection.java
+++ src/main/java/si/lab/rest/StudentsCollection.java
@@ -7,7 +7,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import java.util.Collection;
-import java.util.List;
 
 @Path("students")
 public class StudentsCollection {
