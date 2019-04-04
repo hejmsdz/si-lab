67dd284f58bb4988c20e2eb460f20a2983911e33
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-04-04 10:59:27 +0200

    set up http server

---
+++ src/main/java/si/lab/Main.java
@@ -0,0 +1,16 @@
+package si.lab;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+
+public class Main {
+    public static void main(String[] args) {
+        URI baseUri = UriBuilder.fromUri("http://localhost/").port(9998).build();
+        ResourceConfig config = new ResourceConfig();
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(baseUri, config);
+    }
+}
